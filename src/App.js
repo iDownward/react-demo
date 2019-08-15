53f44408988a72fdb9d70883e1cdb956c3ee95fe
Matheus Henrique Cunha De Oliveir <matheus.oliveira@ibm.com>
2019-08-15 14:55:51 -0300

    commit galho

--- src/App.js
+++ src/App.js
@@ -15,19 +15,19 @@ function App() {
         <a href="https://www.java.com/pt_BR/download/" target="_blank">
         <div className="menuItem">
           <h2>Java</h2>
-          <p>bla bla blaaaa dasodjaso jsaidjasi odasdiuhas uihsa dhaiusdksfjsdljf</p>
+          <p>bla bla blaaaa dasodjaso jsai</p>
         </div>
         </a>
         <a href="https://www.python.org/" target="_blank">
         <div className="menuItem" style={{backgroundColor: '#ff88ff'}}>
           <h2>Paiton</h2>
-          <p>bla bla blaaaa dasodjaso jsaidjasi odasdiuhas uihsa dhaiusdksfjsdljf</p>
+          <p>bla bla blaaaa dsdljf</p>
         </div>
         </a>
         <a href="https://pt.wikipedia.org/wiki/C%2B%2B" target="_blank">
         <div className="menuItem">
           <h2>C mais mais</h2>
-          <p>bla bla blaaaa dasodjaso jsaidjasi odasdiuhas uihsa dhaiusdksfjsdljf</p>
+          <p>bla bla blaaaa dasljf</p>
         </div>
         </a>
       </div>
