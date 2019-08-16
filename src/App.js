2df51248e69554066ef81fdff334c5cd5f71d618
Matheus Henrique Cunha De Oliveir <matheus.oliveira@ibm.com>
2019-08-16 10:30:56 -0300

    sem link

--- src/App.js
+++ src/App.js
@@ -12,24 +12,24 @@ function App() {
         </p>
       </header>
       <div className="Menu">
-        <a href="https://www.java.com/pt_BR/download/" target="_blank">
+
         <div className="menuItem">
           <h2>Java</h2>
-          <p>bla bla blaaaa dasodjaso jsai</p>
+          <p>bla bla blaaaa dasodjaso hjkjhkjhkjhkjsadkjsadkjaSasASasjsai</p>
         </div>
-        </a>
-        <a href="https://www.python.org/" target="_blank">
+
+
         <div className="menuItem" style={{backgroundColor: '#ff88ff'}}>
           <h2>Paiton</h2>
           <p>bla bla blaaaa dsdljf</p>
         </div>
-        </a>
-        <a href="https://pt.wikipedia.org/wiki/C%2B%2B" target="_blank">
+
+
         <div className="menuItem">
           <h2>C mais mais</h2>
           <p>bla bla blaaaa dasljf</p>
         </div>
-        </a>
+
       </div>
     </div>
   );
